a327329f79b64fc60246aac923844aef492be4c7
cooooooolethanhackerz22113 <wangethanhk@gmail.com>
2024-02-24 12:15:42 +0800

    f

--- main.py
+++ main.py
@@ -4,28 +4,46 @@ Q1:
 '''
 
 # TODO: Write your code here
-
+empty_list = [ ]
+words = ["It", "was", "a", "stormy", "night"]
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
+words = ["It", "was", "a", "stormy", "night"]
 
+words.insert(4,"dark")
+words.insert(5,"and")
+
+print(words)
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
+words = ["It", "was", "a", "dark", "and", "stormy", "night"]
 
+words.remove(2,"was")
+words.insert(2,"IS")
+
+print(words)
 ##################################################
 '''
 Q4:
 '''
 
 # TODO: Write your code here
+words = ["It", "IS", "a", "dark", "and", "stormy", "night"]
 
+for w in words:
+    
+    if "a" in w
+        words.remove(w)
+
+print(words)
 ##################################################
 '''
 Q5:
